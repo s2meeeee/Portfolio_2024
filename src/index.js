bdde0171ef9e58152b24dc36b307f429d1be47f5
s2meeeee <s2m9730@gmail.com>
2024-11-30 10:32:03 +0900

    Reconfirm the Scss Connection

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./style.scss"; // 상대 경로 사용
+    
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     filename: "bundle.js", // 번들된 파일 이름
     path: path.resolve(__dirname, "dist"), // 출력 폴더 (dist)
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, // .scss 파일을 처리
+        use: [
+          "style-loader", // HTML에 CSS를 삽입
+          "css-loader", // CSS를 읽어들임
+          "sass-loader", // SCSS를 CSS로 컴파일
+        ],
+      },
+    ],
+  },
   mode: "development", // 개발 모드
 };
